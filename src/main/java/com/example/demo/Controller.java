a9afb7e2d538f5a26c53545944e5b5b1955597a7
LeoKao <luciferstut@gmail.com>
2022-03-31 20:57:26 +0800

    v2

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -14,11 +15,19 @@ public class Controller {
 	@GetMapping(value = "/hello")
 	public String hello(){
 		int i = 1/0;
+
+
+
 		return "hello world!";
 	}
 
 	@GetMapping(value = "/password")
 	public String password(){
 		return password;
+	}
+
+	@GetMapping(value = "/redirect")
+	public RedirectView redirect(@RequestParam("url") String url){
+        return new RedirectView(url); // 重新導向到指定的url
 	}
 }
